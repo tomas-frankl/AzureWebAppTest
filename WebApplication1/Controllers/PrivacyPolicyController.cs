bd648332d8064818a3513df7e0d17340d3e083a0
franktom <tomas.frankl@gmail.com>
2022-11-09 13:11:38 +0100

    PrivacyPolicy page

---
+++ WebApplication1/Controllers/PrivacyPolicyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureWebAppTest.Controllers
+{
+    public class PrivacyPolicyController : Controller
+    {
+        // GET: PrivacyPolicy
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
